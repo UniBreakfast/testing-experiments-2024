d0b56c2429ad010f76c698542161b53b5fc60cd0
UniBreakfast <unibreakfast1@gmail.com>
2024-03-02 12:14:31 +0200

    Refactor addOptions method to handle directives

--- select-with-methods/swm.js
+++ select-with-methods/swm.js
@@ -1,23 +1,37 @@
-export { swm as selectWithMethods, swm as default, swm, reset };
-
+export { swm as selectWithMethods, swm as default, swm, reset }; // project://select-with-methods/select-with-methods.test.js
 let s;
 
 const swm = {
   setSelect(select) {
-    if (!arguments.length) throw new Error('No select passed');
+    if (!(select instanceof HTMLSelectElement) && select !== null) throw new Error('No select passed');
 
     s = select;
   },
 
-  createOptions({items}) {
+  createOptions({ items }) {
     return Array(items.length).fill(document.createElement('option'))
       .map((option, i) => Object.assign(option, { value: items[i].value, textContent: items[i].text }));
   },
 
-  addOptions(options) {
+  addOptions(options, directives) {
     if (!s) throw new Error('No select set');
 
-    s.append(...options);
+    if (!options) throw Error('No options passed');
+
+    if (typeof options[Symbol.iterator] != 'function' || Array.prototype.some.call(options, o => !(o instanceof Option))) {
+      throw new Error('Correct options argument required (iterable with option elements)');
+    }
+
+    const dirKeys = ['before', 'after', 'select', 'mark', 'hide'];
+
+    if (arguments.length > 1 && (!directives || typeof directives != 'object' || Object.keys(directives).filter(key => dirKeys.includes(key)).length < 1)) {
+      throw new Error('Incorrect directives argument is passed');
+    }
+
+    const { before, after, select, mark, hide } = directives || {};
+
+    if (before) s.prepend(...options);
+    else s.append(...options);
   },
 
   getValue() {
